eb01d235b8eac21d043e2a429374c122090a4a20
ChamodJayarathne <148670748+ChamodJayarathne@users.noreply.github.com>
2024-11-04 21:53:30 +0530

    complete part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+<BrowserRouter>
+      <div>
+    
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       "chat-dark": "#2F3136",
       "chat-green": "#00B894",
     },
+    // backgroud:{
+    //   "text-gradient": "linear-gradient(90deg, rgba(9, 127, 77, 0.5) 0%, rgba(9, 127, 77, 0) 100%)"
+
+    // }
   },
   plugins: [],
 };
